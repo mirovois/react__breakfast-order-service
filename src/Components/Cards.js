a6f5df5a4a40eaab2ff5168507331ca1c223b642
Myroslav Voysovych <mirovois@gmail.com>
2020-07-31 23:06:39 -0400

    Modified Order and Card components as well as did some styling

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,51 +1,55 @@
-import React from 'react';
+import React from "react";
 import Card from "./Card";
 import Loading from "./Loading";
 const Cards = (props) => {
-    // const{details:{name,desc,price,image,status},addToOrder,index} = props;
-    const{details:{special,additional},addToOrder,loading,isOrderShown}=props;
-    
-    // console.log(special);
-    return (
+  // const{details:{name,desc,price,image,status},addToOrder,index} = props;
+  const {
+    details: { special, additional },
+    addToOrder,
+    loading,
+    isOrderShown,
+  } = props;
 
-        <div className= {isOrderShown ? "cards-container" : "cards-container full"}>
-          <div className="menu">
-            <div className="special">
-            <h1>Daily Specials</h1>
-            <h1 >Daily Specials</h1>
-            <div className="row special">
-            {Object.keys(special).map(itemID => 
-          <Card 
-            key={itemID} 
-            index = {itemID}
-            details={special[itemID]} 
-            addToOrder={addToOrder}
-            isLargeRow 
-            isOrderShown
-            loading={false}
-            
-            />)}
-            </div>
-            </div>
-           
-            <div className="additional">
-            <h1>Daily Additionals</h1>
-            <div className="row additional">
-            {Object.keys(additional).map(itemID => 
-          <Card 
-            key={itemID} 
-            index = {itemID}
-            details={additional[itemID]} 
-            addToOrder={addToOrder}
-            isOrderShown
-            loading={false}
-            
-            />)}
-            </div>
-            </div>
-         </div>
+  // console.log(special);
+  return (
+    <div className={isOrderShown ? "cards-container" : "cards-container full"}>
+      <div className="menu">
+        <div className="special__wrapper">
+          <h1>Daily Specials</h1>
+
+          <div className="row__special">
+            {Object.keys(special).map((itemID) => (
+              <Card
+                key={itemID}
+                index={itemID}
+                details={special[itemID]}
+                addToOrder={addToOrder}
+                isLargeRow
+                isOrderShown
+                loading={false}
+              />
+            ))}
+          </div>
         </div>
-    )
-}
+
+        <div className="additional__wrapper">
+          <h1>Daily Additionals</h1>
+          <div className="row__additional">
+            {Object.keys(additional).map((itemID) => (
+              <Card
+                key={itemID}
+                index={itemID}
+                details={additional[itemID]}
+                addToOrder={addToOrder}
+                isOrderShown
+                loading={false}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Cards;
--- src/Components/Order.js
+++ src/Components/Order.js
@@ -1,52 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import IconButton from "@material-ui/core/IconButton";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import CloseIcon from "@material-ui/icons/Close";
 
 const Order = (props) => {
-    const limitTitle = (title, limit) => {
-        const newTitle = [];
-        if (title.length >limit) {
-            title.split(' ').reduce((acc,cur) => {
-                if((cur.length +acc)<limit) {
-                     newTitle.push(cur);
-                } return acc+cur.length
-            },0);
-         return `${newTitle.join(' ')}...`
-        }return title;
+  const limitTitle = (title, limit) => {
+    const newTitle = [];
+    if (title.length > limit) {
+      title.split(" ").reduce((acc, cur) => {
+        if (cur.length + acc < limit) {
+          newTitle.push(cur);
+        }
+        return acc + cur.length;
+      }, 0);
+      return `${newTitle.join(" ")}...`;
     }
+    return title;
+  };
 
-    const renderOrder = (orderId) => {
-        const orderedItem = items[orderId];
-        const count = props.order[orderId];
-        return <li>
-           
-            <span>{count} x {orderedItem.name}</span><strong>{formatPrice(count * orderedItem.price)}</strong>
-            <button class="delete__btn"
-            onClick={()=>props.removeFromOrder(orderId)}><DeleteForeverIcon />
-                </button>  
-             </li>
-            }
-
-    const formatPrice=(amount) =>{
-        return amount.toLocaleString("en-CA", {
-            style:"currency",
-            currency:"CAD"
-        });
-    }
-    const items = {...props.details.special,...props.details.additional}
-    const orderIds =Object.keys(props.order);
-    const total = orderIds.reduce((prevTotal,orderId) =>{
-        const orderedItem = items[orderId];
-        const count = props.order[orderId];
-        return prevTotal+(count*orderedItem.price)
-    },0); 
-    
+  const renderOrder = (orderId) => {
+    const orderedItem = items[orderId];
+    const count = props.order[orderId];
     return (
-        <div className={props.isOrderShown ? "order-container" : "order-container hidden"}>
-            <h2 className="order__header">Order</h2>
-            <ul>{orderIds.map(renderOrder)}</ul>
+      <li>
+        <span>
+          {count} x {orderedItem.name}
+        </span>
+        <strong>{formatPrice(count * orderedItem.price)}</strong>
+        <button
+          class="delete__btn"
+          onClick={() => props.removeFromOrder(orderId)}
+        >
+          <DeleteForeverIcon />
+        </button>
+      </li>
+    );
+  };
 
-            {/* <TransitionGroup component="ul">
+  const formatPrice = (amount) => {
+    return amount.toLocaleString("en-CA", {
+      style: "currency",
+      currency: "CAD",
+    });
+  };
+  const items = { ...props.details.special, ...props.details.additional };
+  const orderIds = Object.keys(props.order);
+  const total = orderIds.reduce((prevTotal, orderId) => {
+    const orderedItem = items[orderId];
+    const count = props.order[orderId];
+    return prevTotal + count * orderedItem.price;
+  }, 0);
+
+  return (
+    <div
+      className={
+        props.isOrderShown ? "order-container" : "order-container hidden"
+      }
+    >
+      <h2 className="order__header">Order</h2>
+      <ul>{orderIds.map(renderOrder)}</ul>
+
+      {/* <TransitionGroup component="ul">
             {orderIds.map(orderId =>
                 <CSSTransition 
                     classNames="order" 
@@ -76,14 +91,18 @@ const Order = (props) => {
                 </CSSTransition>
                 )}
             </TransitionGroup> */}
-            <div className="total">
-                Total:{formatPrice(total)}
-            </div>
-            
-            <button  onClick={() => props.closeOrder()}
-                >Close</button>
-        </div>
-    )
-}
+      <div className="order__total">
+        Total:
+        {formatPrice(total)}
+      </div>
+      <IconButton class="closeOrder__btn" onClick={() => props.closeOrder()}>
+        <CloseIcon />
+      </IconButton>
+      {/* <button class="closeOrder__btn" onClick={() => props.closeOrder()}>
+        Close
+      </button> */}
+    </div>
+  );
+};
 
-export default Order
+export default Order;
