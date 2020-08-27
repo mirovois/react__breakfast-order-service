4480b91054dddfdc480b30f2c8728679f5fe01ab
Myroslav Voysovych <mirovois@gmail.com>
2020-08-27 13:51:05 -0400

    modified the Header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React from "react";
 import food_icon from "../images/food_icon.png";
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 const Header = (props) => {
-    return (
-        <header>
-            <img className="header__logo" src={food_icon} alt="food-icon"></img>
-            <h1 className="header__title">Breakfast Order Service</h1>
-            <div className="header__optionBasket">
-                <button class="basket__btn" onClick={()=>props.openBasket()}><ShoppingBasketIcon /></button>
-                {Object.keys(props.order).length>0 && 
-                <span className="header__basketCount">{Object.keys(props.order).length}</span>
-                }
-            </div>
-        </header>
-    )
-}
+  return (
+    <header>
+      <img className="header__logo" src={food_icon} alt="food-icon"></img>
+      <h1 className="header__title">Breakfast Order Service App</h1>
+      <div className="header__optionBasket">
+        <button class="basket__btn" onClick={() => props.openBasket()}>
+          <ShoppingBasketIcon />
+        </button>
+        {Object.keys(props.order).length > 0 && (
+          <span className="header__basketCount">
+            {Object.keys(props.order).length}
+          </span>
+        )}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
